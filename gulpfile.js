ed524ede8ac9e0777953d708393929cb8b1c8089
rjadoo <rjadoo@ipcoop.com>
2016-07-31 12:51:46 -0400

    added compass process

--- gulpfile.js
+++ gulpfile.js
@@ -7,36 +7,42 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var compass = require('gulp-compass');
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('js/*.js')
+    return gulp.src('components/js/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
-// Compile Our Sass
-gulp.task('sass', function() {
-    return gulp.src('scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist/css'));
+// Compile Our Sass using Compass
+gulp.task('compass', function() {
+    return gulp.src('components/sass/style.scss')
+        .pipe(compass({
+        sass: 'components/sass',
+        image: 'builds/development/images',
+        style: 'expanded'
+    }))
+        .pipe(gulp.dest('builds/development/css'));
 });
 
 // Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
+//gulp.task('scripts', function() {
+//    return gulp.src('js/*.js')
+//        .pipe(concat('all.js'))
+//        .pipe(gulp.dest('dist'))
+//        .pipe(rename('all.min.js'))
+//        .pipe(uglify())
+//        .pipe(gulp.dest('dist/js'));
+//});
 
 // Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['lint', 'scripts']);
-    gulp.watch('scss/*.scss', ['sass']);
-});
+//gulp.task('watch', function() {
+//    gulp.watch('js/*.js', ['lint', 'scripts']);
+//    gulp.watch('scss/*.scss', ['sass']);
+//});
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+//gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+gulp.task('default', ['lint', 'sass', 'watch']);
