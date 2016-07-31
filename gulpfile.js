854d97c39ebebe87022914de0567b424c7135ccd
rjadoo <rjadoo@ipcoop.com>
2016-07-31 13:28:58 -0400

    some style changes

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,21 @@ var gulp = require('gulp');
 
 // Include Our Plugins
 var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass'); //using compass instead, so remove later
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var compass = require('gulp-compass');
 
 // Lint Task
-gulp.task('lint', function() {
-    return gulp.src('components/js/*.js')
+gulp.task('lint', function () {
+    return gulp.src('components/scripts/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Compile Our Sass using Compass
-gulp.task('compass', function() {
+gulp.task('compass', function () {
     return gulp.src('components/sass/style.scss')
         .pipe(compass({
         sass: 'components/sass',
@@ -28,21 +28,21 @@ gulp.task('compass', function() {
 });
 
 // Concatenate & Minify JS
-//gulp.task('scripts', function() {
-//    return gulp.src('js/*.js')
-//        .pipe(concat('all.js'))
-//        .pipe(gulp.dest('dist'))
-//        .pipe(rename('all.min.js'))
-//        .pipe(uglify())
-//        .pipe(gulp.dest('dist/js'));
-//});
+gulp.task('scripts', function () {
+    return gulp.src('builds/development/js/*.js')
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('all.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('builds/production/js'));
+});
 
 // Watch Files For Changes
-//gulp.task('watch', function() {
-//    gulp.watch('js/*.js', ['lint', 'scripts']);
-//    gulp.watch('scss/*.scss', ['sass']);
-//});
+gulp.task('watch', function () {
+    gulp.watch('components/scripts/*.js', ['lint', 'scripts']);
+    gulp.watch('components/sass/*.scss', ['compass']);
+});
 
 // Default Task
 //gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
-gulp.task('default', ['lint', 'sass', 'watch']);
+gulp.task('default', ['lint', 'compass', 'watch']);
